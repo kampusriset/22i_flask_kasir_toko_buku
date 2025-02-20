462b4f2acb164edb651f1c2ecd73f368df41d2f5
bagusa77 <bagusaditya7344@gmail.com>
2025-02-20 20:44:58 +0700

    finall

---
+++ team/Isnand.py
@@ -0,0 +1,14 @@
+# Perkenalan Moh. Dzakirin Amrulloh
+def perkenalan():
+    nama = "Isnand Farizky Prabowo"
+    nim = "478"
+    peran = "Pengatur akses aplikasi"
+    deskripsi = (
+        f"Halo, saya {nama} (NIM: {nim}).\n"
+        "Saya bertugas memastikan aplikasi dapat diakses dengan baik oleh pengguna.\n"
+        "Secara sederhana, saya mengatur agar aplikasi selalu online dan siap digunakan kapan saja."
+    )
+    print(deskripsi)
+
+if __name__ == "__main__":
+    perkenalan()
---
+++ team/bagus.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Nama saya:", nama)
+    print("NIM:", nim)
+    print("Peran saya yaitu:", peran)
+
+if __name__ == "__main__":
+    nama = "Bagus Aditya Saputra"
+    nim = "22.130.10.487"
+    peran = "Mengerjakan Script Flask (Developer)"
+    perkenalan(nama, nim, peran)
